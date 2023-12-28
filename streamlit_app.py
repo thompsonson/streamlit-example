d3db532d3f784645bef904e64d6aa460b45006ac
Matt Thompson <thompsonson@gmail.com>
2023-12-28 09:35:36 +0000

    simple agent

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,30 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+from langchain.llms import OpenAI
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.callbacks import StreamlitCallbackHandler
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st_callback = StreamlitCallbackHandler(st.container())
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", type="password")
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+    st.stop()
+else:     
+    llm = OpenAI(openai_api_key=openai_api_key, temperature=0, streaming=True)
+    tools = load_tools(["ddg-search"])
+    agent = initialize_agent(
+        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    )
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+    with st.chat_message("assistant"):
+        st_callback = StreamlitCallbackHandler(st.container())
+        response = agent.run(prompt, callbacks=[st_callback])
+        st.write(response)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
